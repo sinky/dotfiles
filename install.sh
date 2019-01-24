eed31a9663e110a4d6ca724908d541b998239cc9
Marco Krage <marco@my-azur.de>
2019-01-24 07:39:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/env
 
 if [ -f "$HOME/.zshrc" ]; then 
-  echo "Moving $HOME/.zshrc to $HOME/.zshrc.bak"; 
-  mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+  echo "Moving $HOME/.zshrc to $HOME/.zshrc.pre-my-dotfiles"; 
+  mv "$HOME/.zshrc" "$HOME/.zshrc.pre-my-dotfiles"
 fi
 
 ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
